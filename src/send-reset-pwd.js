daa9a789975b87bc9f3f8ca494507a2820deeb7f
rodrigonasif <rodrigonasif@gmail.com>
2019-09-16 10:35:37 -0300

    Fixedtypo in send-reset-pwd

--- src/send-reset-pwd.js
+++ src/send-reset-pwd.js
@@ -11,7 +11,7 @@ const debug = makeDebug('authLocalMgnt:sendResetPwd');
 
 module.exports = sendResetPwd;
 
-async function sendResetPwd (options, identifyUser, notifierOptions, field) {
+async function sendResetPwd(options, identifyUser, notifierOptions, field) {
   debug('sendResetPwd');
   const usersService = options.app.service(options.service);
   const usersServiceIdName = usersService.id;
@@ -19,19 +19,34 @@ async function sendResetPwd (options, identifyUser, notifierOptions, field) {
   ensureObjPropsValid(identifyUser, options.identifyUserProps);
 
   const users = await usersService.find({ query: identifyUser });
-  const user1 = getUserData(users, options.skipIsVerifiedCheck ? [] : ['isVerified', 'hasWaitedToChangePassword'];
+  const user1 = getUserData(
+    users,
+    options.skipIsVerifiedCheck
+      ? []
+      : ['isVerified', 'hasWaitedToChangePassword']
+  );
 
   const user2 = Object.assign(user1, {
     resetExpires: Date.now() + options.resetDelay,
-    resetToken: concatIDAndHash(user1[usersServiceIdName], await getLongToken(options.longTokenLen)),
-    resetShortToken: await getShortToken(options.shortTokenLen, options.shortTokenDigits)
+    resetToken: concatIDAndHash(
+      user1[usersServiceIdName],
+      await getLongToken(options.longTokenLen)
+    ),
+    resetShortToken: await getShortToken(
+      options.shortTokenLen,
+      options.shortTokenDigits
+    )
   });
 
   notifier(options.notifier, 'sendResetPwd', user2, notifierOptions);
   const user3 = await usersService.patch(user2[usersServiceIdName], {
     resetExpires: user2.resetExpires,
     resetToken: await hashPassword(options.app, user2.resetToken, field),
-    resetShortToken: await hashPassword(options.app, user2.resetShortToken, field)
+    resetShortToken: await hashPassword(
+      options.app,
+      user2.resetShortToken,
+      field
+    )
   });
 
   return options.sanitizeUserForClient(user3);
